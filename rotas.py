055c7979f31ad0b78ad29a3b4012613f1d9a3eb2
viniciusmullerdefreitas <viniciusmullerdefreitas309@gmail.com>
2022-07-24 23:16:59 -0300

    Ajustes no arquivo das rotas

--- rotas.py
+++ rotas.py
@@ -1,9 +1,12 @@
-#Variaveis
-Possibilidades = []
-Yep = True
-
 #Funcao de criar possibilidades 
 def makeRote(rote_i, quant, mat):
+    #Variaveis
+    Possibilidades = []
+    Yep = True 
+    Rota = {'Caminho':[],
+            'X':0,
+            'Y':0
+            }
 
     #Verifica se a rota padrao e possivel
     for k in range(quant-1):
@@ -15,7 +18,10 @@ def makeRote(rote_i, quant, mat):
             if(mat[x][y] == 0):
                 Yep = False
     if(Yep):
-        Possibilidades.append(rote_i)            
+        Rota['Caminho'] = rote_i
+        Rota['X'] = 0
+        Rota['Y'] = 0
+        Possibilidades.append(Rota)            
     
     #Gera todas as combinacoes de de rotas
     for i in range(quant-1):
@@ -32,4 +38,9 @@ def makeRote(rote_i, quant, mat):
                     
                     #Verifica se a rota e possivel
                     if(mat[x][y] != 0):
-                        Possibilidades.append(rote_i)
+                        Rota['Caminho'] = rote_i
+                        Rota['X'] = x
+                        Rota['Y'] = y
+                        Possibilidades.append(Rota)
+                        
+    return Possibilidades
